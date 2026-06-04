8774c8e56a79ec6d5331fd06d05cbbfe24235d2a
wpcszsd <as2313496@mail.ustc.edu.cn>
2026-06-04 14:38:56 +0800

    chore: add brainstorming session and logs

---
+++ .superpowers/brainstorm/20260602-214614/start-server.ps1
@@ -0,0 +1,5 @@
+$env:BRAINSTORM_DIR = "H:\jiangshi\.superpowers\brainstorm\20260602-214614"
+$env:BRAINSTORM_HOST = "127.0.0.1"
+$env:BRAINSTORM_URL_HOST = "localhost"
+$serverScript = "C:\Users\wang\.cache\opencode\packages\superpowers@git+https_\github.com\obra\superpowers.git\node_modules\superpowers\skills\brainstorming\scripts\server.cjs"
+node $serverScript
